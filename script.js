b0c4f6d17a56431db036d7db0641c00d8c8cc56d
Harris-PS <psharris2002@gmail.com>
2025-12-05 10:59:04 +0530

    added hover effect to the cells

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const container = document.getElementById('container');
 container.textContent = "Etch-A-Sketch";
 
-// creating the 16x16 grid
 function createGrid(size){
   
   
@@ -9,14 +8,21 @@ function createGrid(size){
     let row = document.createElement('div');
     row.className = "row";
     container.appendChild(row);
-  
+
 
   for(let j=0;j<size;j++){  
       let cell = document.createElement("div");
       cell.className = "cell";
       row.appendChild(cell);
+      cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = 'yellow';
+      }); 
+      cell.addEventListener('mouseout', () => {
+      cell.style.backgroundColor = '';
+    });
     }
-   }
   }
+}
+
 
 createGrid(16);
