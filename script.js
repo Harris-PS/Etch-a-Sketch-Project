d933185f9c675f4a235f134ea2149591630bb874
Harris-PS <psharris2002@gmail.com>
2025-12-05 12:42:27 +0530

    Made changes to the button style

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function createGrid(size){
   const container = document.getElementById('container');
   container.textContent = "Etch-A-Sketch";
 
+  // Function to Change Grid Size
   const gridsizebtn = document.createElement('button');
   gridsizebtn.className = "gridsizebtn";
   gridsizebtn.textContent = "Change Grid Size";
   container.appendChild(gridsizebtn);
 
-  // Function to create grid size
   function gridsize(){
     let gridsize = prompt("Enter grid size between 3 to 100");
 
@@ -16,28 +16,27 @@ function createGrid(size){
     if(gridsize < 3 || gridsize > 100){
       alert("Please enter a number between 3 to 100");
       return;
-    } 
+    }
 
     container.textContent = "";
 
     createGrid(gridsize);
   }
-
   gridsizebtn.addEventListener('click', gridsize);
 
+  // Function to Reset Grid
   const resetbtn = document.createElement('button');
   resetbtn.className = "resetbtn";
   resetbtn.textContent = "Reset Grid";
+  container.appendChild(resetbtn);
 
   function reset(){
     container.textContent = "";
     createGrid(size);
   }
-
   resetbtn.addEventListener('click', reset);
-  container.appendChild(resetbtn);
 
-
+  // For loop to Create Grid
   for(let i=0;i<size;i++){
     let row = document.createElement('div');
     row.className = "row";
@@ -53,7 +52,7 @@ function createGrid(size){
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
         cell.style.backgroundColor = `rgb(${r},${g},${b})`;
-      }); 
+      });
     }
   }
 }
