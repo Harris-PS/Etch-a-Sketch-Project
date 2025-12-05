3d18ec9a67d8e95120b36a13b5e299c7017add53
Harris-PS <psharris2002@gmail.com>
2025-12-05 11:53:40 +0530

    Added button to change the grid size

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const container = document.getElementById('container');
-container.textContent = "Etch-A-Sketch";
-
-
 function createGrid(size){
-  
+  const container = document.getElementById('container');
+  container.textContent = "Etch-A-Sketch";
+
+  const gridsizebtn = document.createElement('button');
+  gridsizebtn.textContent = "Change Grid Size";
+  container.appendChild(gridsizebtn);
+
+  function gridsize(){
+    let gridsize = prompt("Enter grid size between 3 to 100");
+
+    gridsize = parseInt(gridsize);
+
+    container.textContent = "";
+
+    createGrid(gridsize);
+  }
+
+  gridsizebtn.addEventListener('click', gridsize);
+
+
   for(let i=0;i<size;i++){
     let row = document.createElement('div');
     row.className = "row";
@@ -18,7 +33,7 @@ function createGrid(size){
         const r = Math.floor(Math.random() * 256);
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
-        cell.style.backgroundColor = `rgba(${r},${g},${b})`;
+        cell.style.backgroundColor = `rgb(${r},${g},${b})`;
       }); 
     }
   }
